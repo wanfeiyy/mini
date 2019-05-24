7b565f4e88f8413a2611b7a650dcbfbca08704b8
wanfeiyi <wanfeiyi@medlinker.com>
2019-05-24 16:42:30 +0800

    fix

--- app/Services/MiniWechatService.php
+++ app/Services/MiniWechatService.php
@@ -24,7 +24,7 @@ class MiniWechatService
         try {
            $ret = $this->app->auth->session($jsCode);
            //return ['session_key' => 'xxxxxx', 'openid' => mt_rand(1,10000)];
-           if (isset($ret['errcode']) && $ret['errcode'] === 0) {
+           if (! isset($ret['errcode']) || $ret['errcode'] != 0) {
                return $ret;
            } else {
                throw new \Exception('auth.code2Session error:' . json_encode($ret));
